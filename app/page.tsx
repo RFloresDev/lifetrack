145e26eba5afb1b1248217c5026fefa52b1bfdf7
Richard <lcdo.richardflores@gmail.com>
2025-02-12 17:35:53 -0300

    eliminando archivos innecesarios y mejorando estructura

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,35 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import DeployButton from "@/components/deploy-button";
+import AuthButton from "@/components/header-auth";
+import { Link } from "lucide-react";
 
 export default async function Home() {
-  return (
-    <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
-  );
+	return (
+		<div className="flex-1 w-full flex flex-col gap-20 items-center">
+			<nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+				<div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+					<DeployButton />
+					{<AuthButton />}
+				</div>
+			</nav>
+			<div className="fixed inset-0 z-[-1]"></div>
+			<div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3 z-0">
+				<main className="flex-1 flex flex-col gap-6 border p-4 text-lg">
+					<Link href="/dashboard"></Link>
+				</main>
+			</div>
+
+			<footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+				<p>
+					Powered by{" "}
+					<a
+						href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+						target="_blank"
+						className="font-bold hover:underline"
+						rel="noreferrer">
+						Supabase
+					</a>
+				</p>
+			</footer>
+		</div>
+	);
 }
